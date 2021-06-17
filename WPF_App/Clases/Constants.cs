56b19acbeac6c2cb8839551c2be5af4540a83ced
wilmilcard <leonjuandavid@hotmail.com>
2021-06-17 18:28:46 -0500

    Commands y ventana

--- WPF_App/Clases/Constants.cs
+++ WPF_App/Clases/Constants.cs
@@ -14,7 +14,7 @@ namespace WPF_App.Clases
 
         public const string apikey = "77b14e0dd13289e6f9ea07fd29169c36";
         public const string hash = "d83a9612c2a47bece6ccfcb46bd5553d";
-        public const string credenciales = "?ts=1&apikey=" + apikey + "&hash=" + hash;
+        public const string credenciales = "?ts=1&limit=100&apikey=" + apikey + "&hash=" + hash;
         
         public const string comics = "v1/public/comics";
         public const string characters = "v1/public/characters";
--- WPF_App/ViewModels/ComicsViewModel.cs
+++ WPF_App/ViewModels/ComicsViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using WPF_App.Clases;
 using WPF_App.Models;
 
@@ -16,17 +18,27 @@ namespace WPF_App.ViewModels
         private ObservableCollection<Comic> _listaComics = new ObservableCollection<Comic>();
         public ObservableCollection<Comic> ListaComics { get { return _listaComics; } set { _listaComics = value; OnPropertyChanged("ListaComics"); } }
 
+        private ICommand _exportarCommand, _buscarCommand;
+        public ICommand ExportarCommand { get { if (_exportarCommand == null) _exportarCommand = new RelayCommand(new Action(Exportar)); return _exportarCommand; } }
+        public ICommand BuscarCommand { get { if (_buscarCommand == null) _buscarCommand = new RelayCommand(new Action(Consulta_Comics)); return _buscarCommand; } }
+
         public ComicsViewModel()
         {
             this.Consulta_Comics();
         }
 
-        public async void Consulta_Comics(string nombre_comic = "")
+        public async void Consulta_Comics()
         {
             var comics = await con.Get<ComicDataWrapper>();
             
             foreach(var comic in comics.data.results)
                 this.ListaComics.Add(comic);
+
+            this.ListaComics.OrderBy(x => x.title);
+        }
+
+        public async void Exportar()
+        {
 
         }
     }
