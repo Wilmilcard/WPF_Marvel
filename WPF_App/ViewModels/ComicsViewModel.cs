a80f8b960bebcde89801d0e9c85df189c3bc5881
wilmilcard <leonjuandavid@hotmail.com>
2021-06-17 19:43:31 -0500

    Mejoras

--- WPF_App/ViewModels/ComicsViewModel.cs
+++ WPF_App/ViewModels/ComicsViewModel.cs
@@ -19,14 +19,20 @@ namespace WPF_App.ViewModels
         private bool _isLoad = false;
         public bool IsLoad { get { return _isLoad; } set { _isLoad = value; OnPropertyChanged("IsLoad"); } }
 
+        private int _selectedIndex = 0;
+        public int SelectedIndex { get { return _selectedIndex; } set { _selectedIndex = value; OnPropertyChanged("SelectedIndex"); } }
+
         private ObservableCollection<Comic> _listaComics = new ObservableCollection<Comic>();
         public ObservableCollection<Comic> ListaComics { get { return _listaComics; } set { _listaComics = value; OnPropertyChanged("ListaComics"); } }
 
-        private ICommand _exportarCommand, _buscarCommand, _ascCommand, _descCommand;
+        public enum tiposFiltros { titulo, paginas, serie, fecha}
+
+        private ICommand _exportarCommand, _buscarCommand, _ascCommand, _descCommand, _filtroCommand;
         public ICommand ExportarCommand { get { if (_exportarCommand == null) _exportarCommand = new RelayCommand(new Action(Exportar)); return _exportarCommand; } }
         public ICommand BuscarCommand { get { if (_buscarCommand == null) _buscarCommand = new RelayCommand(new Action(Consulta_Comics)); return _buscarCommand; } }
         public ICommand AscCommand { get { if (_ascCommand == null) _ascCommand = new RelayCommand(new Action(ASC)); return _ascCommand; } }
         public ICommand DescCommand { get { if (_descCommand == null) _descCommand = new RelayCommand(new Action(DESC)); return _descCommand; } }
+        public ICommand FiltroCommand { get { if (_filtroCommand == null) _filtroCommand = new RelayCommand(new Action(Filtro)); return _filtroCommand; } }
 
         public ComicsViewModel()
         {
@@ -51,6 +57,12 @@ namespace WPF_App.ViewModels
         public async void Exportar()
         {
 
+        }
+
+        public void Filtro()
+        {
+            int index = this.SelectedIndex;
+            MessageBox.Show(Convert.ToString((int)tiposFiltros.titulo));
         }
 
         public void ASC()
