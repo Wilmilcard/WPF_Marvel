9e02f91480f4b65fadafca3abc724c213c42d672
wilmilcard <leonjuandavid@hotmail.com>
2021-06-21 17:09:26 -0500

    Orden Ascendente y descendente

--- WPF_App/ViewModels/ComicsViewModel.cs
+++ WPF_App/ViewModels/ComicsViewModel.cs
@@ -16,23 +16,25 @@ namespace WPF_App.ViewModels
     {
         Conexion con = new Conexion();
 
-        private bool _isLoad = false;
+        private bool _isLoad = false, esAcs = false;
         public bool IsLoad { get { return _isLoad; } set { _isLoad = value; OnPropertyChanged("IsLoad"); } }
 
-        private int _selectedIndex = 0;
-        public int SelectedIndex { get { return _selectedIndex; } set { _selectedIndex = value; OnPropertyChanged("SelectedIndex"); } }
+        private int _tipoFiltroSeleccionado = 0, OrdenadoPor;
+        public int TipoFiltroSeleccionado { get { return _tipoFiltroSeleccionado; } set { _tipoFiltroSeleccionado = value; OnPropertyChanged("TipoFiltroSeleccionado"); } }
 
         private ObservableCollection<Comic> _listaComics = new ObservableCollection<Comic>();
         public ObservableCollection<Comic> ListaComics { get { return _listaComics; } set { _listaComics = value; OnPropertyChanged("ListaComics"); } }
 
-        public enum tiposFiltros { titulo, paginas, serie, fecha}
-
-        private ICommand _exportarCommand, _buscarCommand, _ascCommand, _descCommand, _filtroCommand;
+        private ICommand _exportarCommand, _buscarCommand, _ascCommand, _descCommand, _filtroCommand, _filtroTitulosCommand, _filtroPaginasCommand, _filtroSeriesCommand, _filtroFechasCommand;
         public ICommand ExportarCommand { get { if (_exportarCommand == null) _exportarCommand = new RelayCommand(new Action(Exportar)); return _exportarCommand; } }
         public ICommand BuscarCommand { get { if (_buscarCommand == null) _buscarCommand = new RelayCommand(new Action(Consulta_Comics)); return _buscarCommand; } }
         public ICommand AscCommand { get { if (_ascCommand == null) _ascCommand = new RelayCommand(new Action(ASC)); return _ascCommand; } }
         public ICommand DescCommand { get { if (_descCommand == null) _descCommand = new RelayCommand(new Action(DESC)); return _descCommand; } }
         public ICommand FiltroCommand { get { if (_filtroCommand == null) _filtroCommand = new RelayCommand(new Action(Filtro)); return _filtroCommand; } }
+        public ICommand FiltroTitulosCommand { get { if (_filtroTitulosCommand == null) _filtroTitulosCommand = new RelayCommand(new Action(esTitulo)); return _filtroTitulosCommand; } }
+        public ICommand FiltroPaginasCommand { get { if (_filtroPaginasCommand == null) _filtroPaginasCommand = new RelayCommand(new Action(esPagina)); return _filtroPaginasCommand; } }
+        public ICommand FiltroSeriesCommand { get { if (_filtroSeriesCommand == null) _filtroSeriesCommand = new RelayCommand(new Action(esSerie)); return _filtroSeriesCommand; } }
+        public ICommand FiltroFechasCommand { get { if (_filtroFechasCommand == null) _filtroFechasCommand = new RelayCommand(new Action(esFecha)); return _filtroFechasCommand; } }
 
         public ComicsViewModel()
         {
@@ -42,14 +44,14 @@ namespace WPF_App.ViewModels
         public async void Consulta_Comics()
         {
             this.IsLoad = true;
-            
+
             var comics = await con.Get<ComicDataWrapper>();
-            
-            foreach(var comic in comics.data.results)
+
+            foreach (var comic in comics.data.results)
                 this.ListaComics.Add(comic);
-            
+
             this.ASC();
-            
+
             this.IsLoad = false;
 
         }
@@ -61,20 +63,46 @@ namespace WPF_App.ViewModels
 
         public void Filtro()
         {
-            int index = this.SelectedIndex;
-            MessageBox.Show(Convert.ToString((int)tiposFiltros.titulo));
+            switch (this.TipoFiltroSeleccionado)
+            {
+                case 0:
+                    if (this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderBy(x => x.title));
+                    if (!this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderByDescending(x => x.title));
+                    break;
+                case 1:
+                    if (this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderBy(x => x.pageCount));
+                    if (!this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderByDescending(x => x.pageCount));
+                    break;
+                case 2:
+                    if (this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderBy(x => x.series.name));
+                    if (!this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderByDescending(x => x.series.name));
+                    break;
+                case 3:
+                    if (this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderBy(x => x.modified));
+                    if (!this.esAcs)
+                        this.ListaComics = new ObservableCollection<Comic>(this.ListaComics.OrderByDescending(x => x.modified));
+                    break;
+            }
+
         }
 
-        public void ASC()
-        {
-            var list = this.ListaComics.OrderBy(x => x.pageCount);
-            this.ListaComics = new ObservableCollection<Comic>(list);
-        }
+        public void ASC() { this.esAcs = true; this.Filtro(); }
 
-        public void DESC()
-        {
-            var list = this.ListaComics.OrderByDescending(x => x.pageCount);
-            this.ListaComics = new ObservableCollection<Comic>(list);
-        }
+        public void DESC() { this.esAcs = false; this.Filtro(); }
+
+        public void esTitulo() { this.Filtro(); this.Filtro(); }
+
+        public void esPagina() { this.Filtro(); this.Filtro(); }
+
+        public void esSerie() { this.Filtro(); this.Filtro(); }
+
+        public void esFecha() { this.Filtro(); this.Filtro(); }
     }
 }
