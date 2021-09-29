76ffe86367c441a299e1a94208a0397e6bc4a820
Wilmilcard <leonjuandavid@hotmail.com>
2021-09-29 15:18:17 -0500

    Implementacion de ViewModelLocator

---
+++ WPF_App/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Ioc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_App.ViewModels
+{
+    public class ViewModelLocator
+    {
+        private static readonly ComicsViewModel comicsViewModel;
+        private static readonly HeroesViewModel heroesViewModel;
+
+        static ViewModelLocator()
+        {
+            comicsViewModel = new ComicsViewModel();
+            heroesViewModel = new HeroesViewModel();
+        }
+
+        public static ComicsViewModel ComicsViewModel => comicsViewModel;
+        public static HeroesViewModel HeroesViewModel => heroesViewModel;
+    }
+}
