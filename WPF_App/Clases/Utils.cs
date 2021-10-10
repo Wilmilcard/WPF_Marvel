abd0422cffa05547c943c8e8f6878e659540cf14
Wilmilcard <leonjuandavid@hotmail.com>
2021-10-09 19:09:13 -0500

    reajuste

--- WPF_App/Clases/Utils.cs
+++ WPF_App/Clases/Utils.cs
@@ -43,26 +43,6 @@ namespace WPF_App.Clases
         }
     }
 
-    public class Archivo
-    {
-        public int height { set; get; }
-        public DateTime timestamp { set; get; }
-        public string uri { set; get; }
-        public string fileName { set; get; }
-        public string data { set; get; }
-
-        public static string SaveFile(string Path, string FileName, string dataFile)
-        {
-            if (System.IO.File.Exists(Path + FileName))
-            {
-                FileName = Globals.GetFechaActual().Ticks.ToString() + FileName;
-            }
-            Byte[] bytes = Convert.FromBase64String(dataFile);
-            File.WriteAllBytes(Path + FileName, bytes);
-            return FileName;
-        }
-    }
-
     public static class Globals
     {
         public static DateTime GetFechaActual()
